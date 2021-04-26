ade07a7738b5034c99e4c80ab141cee8bb2c9d86
Michael <mlarock711@comcast.net>
2021-04-26 00:25:32 -0400

    Did what I could with css and this is what I will turn it.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const app = express()
 const showdata = require('./showdata')
-
-app.set('view engine', 'pug')
+const app = express()
 
 app.use(express.static('public'))
+
+app.set('view engine', 'pug')
 
 app.get('/', (request, response) => {
   return response.render('index', { showdata })
